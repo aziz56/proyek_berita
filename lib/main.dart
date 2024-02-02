ba16f6a627efea73478faa54774f2fe9950459aa
Muhammad Abdul Aziz <BSI80217@bsi.co.id>
2024-02-02 07:43:58 +0700

    renaming dir

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
-import 'package:proyek_berita/datasource.dart';
+import 'package:proyek_berita/App.dart';
 
 void main() async {
   await Hive.initFlutter();
